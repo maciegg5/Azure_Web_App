ed1b82928a70fb6f917f7589f2258ad4e23c6d52
maciegg5 <maciekkrakow2012@gmail.com>
2025-04-25 15:06:23 +0200

    Updated site

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function showSection(id) {
-    document.querySelectorAll('section').forEach(section => {
+    document.querySelectorAll('.section').forEach(section => {
       section.classList.remove('active');
     });
     document.getElementById(id).classList.add('active');
   }
-  
